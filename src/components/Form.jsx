59e65a2100be5dc63a23fd993609c00f9bf07353
codeofsanchie <sanchit.gaikwad07@gmail.com>
2024-04-05 11:17:19 +0530

    Updated design and Merged with Backend

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
-import logo from "../assets/SPITlogo.png"
+import logo from "../assets/Spit.png"
+import "../components/styles.css";
 
 class StudentInformation extends React.Component {
     state = {
@@ -133,38 +134,40 @@ class StudentInformation extends React.Component {
 
             <>
             
-            <div className="container-fluid" style={{ height: "100vh" }}>
-                <div className="row justify-content-center align-items-center h-100">
-                    <div className="col-md-8">
+            <div className="container-fluid" style={{ height: "100%", backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp)', backgroundSize: 'inherit' }}>
+                    <div className="row justify-content-center align-items-center h-100">
+                        <div className="col-md-8" style={{ boxShadow: "white 0px 1px 70px", margin: "20px" }}>
 
-                <div className=" d-flex justify-content-center align-items-center mt-4 bg-light">
-                <img src={logo} alt="spit logo" className="img-fluid"  style={{ width: "750px" }} />
-                </div>
+                            <div className="" style={{ color: "white", border: " 10px solid black", backgroundColor: "#1d1919" }}>
 
-            <div className="text-center mt-3 bg-light">
-               <h2 className="mt-3 fs-4 fw-bold">DEPARTMENT OF COMPUTER ENGINEERING</h2>
-                  <h2 className="fs-5 fw-bolder ">STUDENT INFORMATION FORM</h2>
-            </div>
+                                <div className=" d-flex justify-content-center align-items-center mt-4">
+                                    <img src={logo} alt="spit logo" className="img-fluid" style={{ width: "900px" }} />
+                                </div>
 
-            <div className=" mt-4 row mb-3 bg-light">
-                
-        <form onSubmit={this.handleSubmit}>  {/* To handle form submission to database  */}
-        <div className="row mb-3 justify-content-center">
-                <div className="col-md-4">
-                    <label className="col-form-label text-end">SEM:</label>
-                    <input type="text" className="form-control" name="sem" value={this.state.sem} onChange={this.handleChange}/>
-                </div>
+                                <div className="text-center mt-3">
+                                    <h2 className="mt-3 fs-4 fw-bold underline" >DEPARTMENT OF COMPUTER ENGINEERING</h2>
+                                    <h2 className="fs-4 fw-bolder underline">STUDENT INFORMATION FORM</h2>
+                                </div>
+                            </div>
 
-                <div className="col-md-4">
-                    <label className="col-form-label text-end">CLASS:</label>
-                    <input type="text" className="form-control" name="class" value={this.state.class} onChange={this.handleChange}/>
-                </div>
-                
-                <div className="col-md-4">
-                    <label className="col-form-label text-end">UID NO:</label>
-                    <input type="text" className="form-control" name="uid" value={this.state.uid} onChange={this.handleChange}/>
-                </div>
-            </div>
+                            <form onSubmit={this.handleSubmit} className="custom-form" style={{ margin: "7px" }}>
+                                <div className=" mt-4 row mb-3 bg-light" >
+                                    <div className="row mb-3 justify-content-center">
+                                        <div className="col-md-4">
+                                            <label className="col-form-label text-end">SEM:</label>
+                                            <input type="text" className="form-control" name="sem" value={this.state.sem} onChange={this.handleChange}/>
+                                        </div>
+
+                                        <div className="col-md-4">
+                                            <label className="col-form-label text-end">CLASS:</label>
+                                            <input type="text" className="form-control" name="class" value={this.state.class} onChange={this.handleChange}/>
+                                        </div>
+
+                                        <div className="col-md-4">
+                                            <label className="col-form-label text-end">UID NO:</label>
+                                            <input type="text" className="form-control" name="uid" value={this.state.uid} onChange={this.handleChange}/>
+                                        </div>
+                                    </div>
 
           <div className="row mb-3">
                 <div className="col-md-12">
@@ -656,10 +659,11 @@ class StudentInformation extends React.Component {
             </div>
           
           <button type="submit" className="btn btn-primary container d-flex justify-content-center align-content-center">Submit</button>
+        </div>
         </form>
       </div>
      </div>
-</div></div>
+</div>
             </>
         );
     }
